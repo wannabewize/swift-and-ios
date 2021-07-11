634987b8edbe40b6a82ebcd442f747f27f5f588b
wannabewize <wannabewize@gmail.com>
2021-07-12 05:40:19 +0900

    05장 예제 작성 중

---
+++ 05.ValueChanger/ValueChanger/ValueChanger/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ValueChanger
+//
+//  Created by Jaehoon Lee on 2021/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    // 스테퍼 변경
+    @IBAction func onStepperChanged(_ sender: Any) {
+        // 슬라이더에 반영
+    }
+    
+    @IBAction func onDownClicked(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func onUpClicked(_ sender: Any) {
+    }
+    
+    @IBAction func onSliderChanged(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
