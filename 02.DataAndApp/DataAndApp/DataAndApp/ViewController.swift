e1b14b2422a7657d6ed7c29317ff3393a404a0d0
wannabewize <wannabewize@gmail.com>
2021-07-12 05:10:44 +0900

    데이터, 아웃렛 출력 앱

--- 02.DataAndApp/DataAndApp/DataAndApp/ViewController.swift
+++ 02.DataAndApp/DataAndApp/DataAndApp/ViewController.swift
@@ -7,13 +7,5 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
+₩
 
